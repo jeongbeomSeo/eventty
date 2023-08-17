23ee636b92ebdc057d8d661e8a7074cf53771ab8
jeonbeomseo <jeongbeom4693@gmail.com>
2023-08-17 14:34:31 +0900

    chore: setting

---
+++ src/userservice/src/main/java/com/eventty/userservice/UserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.eventty.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+}
