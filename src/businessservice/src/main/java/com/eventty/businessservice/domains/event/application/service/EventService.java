1ffb4f7da404b3213a02cd8900f130c4c10ddc6a
serinryu <serin0404@naver.com>
2023-09-15 11:50:04 +0900

    fix : [백] 이벤트 검색시 최신순으로 가져오기

--- src/businessservice/src/main/java/com/eventty/businessservice/domains/event/application/service/EventService.java
+++ src/businessservice/src/main/java/com/eventty/businessservice/domains/event/application/service/EventService.java
@@ -21,18 +21,12 @@ public class EventService {
     private final EventBasicRepository eventBasicRepository;
     private final EventSubService eventSubService; // 복잡한 로직의 경우 하위 서비스 호출 (Facade 패턴)
 
-    /**
-     * 이벤트 상세 조회 (이벤트에 대한 모든 정보 + 티켓 정보)
-     */
     public EventWithTicketsFindByIdResponseDTO findEventById(Long eventId){
         // 조회수 증가 (비동기로 처리)
         eventSubService.increaseView(eventId);
         return eventSubService.findEventById(eventId);
     }
 
-    /**
-     * 이벤트 전체 조회
-     */
     @Transactional(readOnly = true)
     public List<EventBasicFindAllResponseDTO> findAllEvents() {
         return Optional.ofNullable(eventBasicRepository.selectAllEvents())
@@ -42,30 +36,18 @@ public class EventService {
                 .orElseThrow(()->EventNotFoundException.EXCEPTION);
     }
 
-    /**
-     * 이벤트 생성
-     */
     public Long createEvent(EventCreateRequestDTO eventCreateRequestDTO){
         return eventSubService.createEvent(eventCreateRequestDTO);
     }
 
-    /**
-     * 이벤트 수정
-     */
     public Long updateEvent(Long id, EventUpdateRequestDTO eventUpdateRequestDTO){
         return eventSubService.updateEvent(id, eventUpdateRequestDTO);
     }
 
-    /**
-     * 이벤트 삭제
-     */
     public Long deleteEvent(Long id){
         return eventSubService.deleteEvent(id);
     }
 
-    /**
-     * 이벤트 카테고리별 조회
-     */
     @Transactional(readOnly = true)
     public List<EventBasicFindAllResponseDTO> findEventsByCategory(Long categoryId){
 
@@ -82,9 +64,6 @@ public class EventService {
 
     }
 
-    /**
-     * 행사 검색
-     */
     @Transactional(readOnly = true)
     public List<EventBasicFindAllResponseDTO> findEventsBySearch(String keyword){
         return Optional.ofNullable(eventBasicRepository.selectEventsBySearch(keyword))
