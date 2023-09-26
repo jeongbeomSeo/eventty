d97506b1bf655bf8459ce22cba5b305e05cb550d
HyeonG <kilhg0623@gmail.com>
2023-09-26 18:46:59 +0900

    fix: [백] 생성자 제거

--- src/applyservice/src/main/java/com/eventty/applyservice/application/dto/request/FindApplicantsListRequestDTO.java
+++ src/applyservice/src/main/java/com/eventty/applyservice/application/dto/request/FindApplicantsListRequestDTO.java
@@ -20,17 +20,4 @@ public class FindApplicantsListRequestDTO {
     private LocalDateTime dateMax;
     private String phone;
     private Long order;
-
-    public FindApplicantsListRequestDTO(){
-        eventId = 1L;
-        state = 0L;
-        applyId = 2L;
-        priceMin = 100L;
-        priceMax = 50000000L;
-        name = "하이";
-        dateMin = LocalDateTime.of(2023, 1, 5, 00, 01, 01);
-        dateMax = LocalDateTime.of(2023, 12, 31, 00, 00, 01);
-        phone = "010-7895-7895";
-        order = 0L;
-    }
 }
