92b0005f05151c23051e5ff3eae98499c7ff2e0d
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-04-06 12:06:16 +0900

    test: 회원가입 병렬적 요청 분석 완료 DB에 Commit되는 과정에서 최초 1회 요청을 제외한 모든 요청이 Rollback되는 것을 확인

--- src/authservice/src/test/java/com/eventty/authservice/presentation/AuthControllerTest.java
+++ src/authservice/src/test/java/com/eventty/authservice/presentation/AuthControllerTest.java
@@ -71,6 +71,9 @@ public class AuthControllerTest {
             // Given
             String fullUserCreateRequestDTOJson = objectMapper.writeValueAsString(fullUserCreateRequestDTO);
 
+            ResponseEntity<ResponseDTO<Void>> response = ResponseEntity.ok(ResponseDTO.of(true));
+            doReturn(response).when(apiClient).createUserApi(any(UserCreateApiRequestDTO.class));
+
             // 5번의 순차적 요청
             // When && Then
             for (int i = 0; i < 5; i++) {
@@ -87,14 +90,20 @@ public class AuthControllerTest {
         }
 
         /*
-        현재 500 에러가 뜨고, 전혀 예상하지 못한 방향으로 결과가 나왔음.
+        executorService.invokeAll(tasks)을 실행했을 때 이미 모든 스레드가 실행되는데, get()을 통해 불필요하게 결과를 받아오려고 해서 500 에러가 발생한 것으로 추정.
+        로직을 분석해볼 결과, @Transactional의 기능이 동작하여 모든 서비스 로직이 종료된 후 DB에 Commit이 되기 때문에 도중 과정은 모두 성공한다.
+        하지만 최종적으로 DB에 Commit될 때 처음 Commit된 요청 외에는 전부 SqlExceptionHelper를 발생한다.
+        이유는 Email을 Unique하게 걸어두었기 때문에 DB에 반영되는 과정에서 오류가 발생한 것으로 추정
          */
 
-        @DisplayName("[?] 회원가입 병렬적 요청")
+        @DisplayName("[성공] 회원가입 병렬적 요청 - 최대 1번의 요청만 반영")
         @Test
         void createUser_Concurrent() throws Exception {
             // Given
             String fullUserCreateRequestDTOJson = objectMapper.writeValueAsString(fullUserCreateRequestDTO);
+
+            ResponseEntity<ResponseDTO<Void>> response = ResponseEntity.ok(ResponseDTO.of(true));
+            doReturn(response).when(apiClient).createUserApi(any(UserCreateApiRequestDTO.class));
 
             int numberOfRequests = 10;
             ExecutorService executorService = Executors.newFixedThreadPool(numberOfRequests);
@@ -120,9 +129,9 @@ public class AuthControllerTest {
             List<Future<Void>> futures = executorService.invokeAll(tasks);
 
             // Then
-            for (Future<Void> future : futures) {
-                future.get();
-            }
+//            for (Future<Void> future : futures) {
+//                future.get();
+//            }
 
             executorService.shutdown();
         }
